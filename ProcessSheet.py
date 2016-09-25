0b29307f058d1ba230ce92b7880a252161b911d1
DESKTOP-4PLSSQM\deado <deadoggy@aliyun.com>
2016-09-25 13:29:06 +0800

    no message

--- ProcessSheet.py
+++ ProcessSheet.py
@@ -17,8 +17,38 @@ class SheetController:
         :return:
         '''
 
-        response = urllib2.urlopen(url)
+        response = urllib2.urlopen(url + '.shtml')
         html = response.read().decode('gbk')
-        PageCount = int(re.findall('''var _PAGE_COUNT="(.*?)"''', html)[0])
+        pagecount = int(re.findall('''var _PAGE_COUNT="(.*?)"''', html)[0])
 
-        
+        SheetContent = ""
+
+        for index in range(0, pagecount):
+            if 0 == index:
+                pageurl = url + '.shtml'
+            else:
+                pageurl = url + '_' + str(index) + '.shtml'
+
+
+    def __processPerSheet(self, url):
+        '''
+        处理每一页的内容（政治和英语）
+        :param url: 该页的url
+        :return:
+        '''
+        ret = ""
+
+        html = urllib2.urlopen(url).read().decode('gbk')
+        pattern_1 = '<div class="TRS_Editor">(.*?)</div>'
+        pattern_2 = '<p align="(.*?)">(.*?)</p>'
+
+        editor = re.findall(pattern_1, html)
+        contentset = re.findall(pattern_2, editor)
+
+        for tag in contentset:
+            if()
+
+
+
+
+
